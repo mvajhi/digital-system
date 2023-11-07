799cf911fef2c0c53f0b8fd7262c7fc05b0e505c
mahdi vajhi <mvajhimv@gmail.com>
2023-11-07 18:58:38 +0330

    Update

--- ca1/tb.sv
+++ ca1/tb.sv
@@ -5,17 +5,17 @@ module tb();
     q1 mygate(.y0(out0), .y1(out1), .a(aa), .b(bb), .c(cc));
     initial begin
         // logic table
-        // #100 aa=0;bb=0;cc=0;
-        // #100 aa=0;bb=0;cc=1;
-        // #100 aa=0;bb=1;cc=0;
-        // #100 aa=0;bb=1;cc=1;
-        // #100 aa=1;bb=0;cc=0;
-        // #100 aa=1;bb=0;cc=1;
-        // #100 aa=1;bb=1;cc=0;
-        // #100 aa=1;bb=1;cc=1;
+        #100 aa=0;bb=0;cc=0;
+        #100 aa=0;bb=0;cc=1;
+        #100 aa=0;bb=1;cc=0;
+        #100 aa=0;bb=1;cc=1;
+        #100 aa=1;bb=0;cc=0;
+        #100 aa=1;bb=0;cc=1;
+        #100 aa=1;bb=1;cc=0;
+        #100 aa=1;bb=1;cc=1;
 
         //0 for y1
-        #100 aa=0;bb=0;cc=0;
+        // #100 aa=0;bb=0;cc=0;
         // #100 aa=0;bb=0;cc=1;
         // #100 aa=1;bb=1;cc=0;
         // #100 aa=1;bb=0;cc=0;
@@ -35,7 +35,7 @@ module tb();
         //1 for y0
         // #100 aa=0;bb=0;cc=1;
         // #100 aa=0;bb=1;cc=0;
-        #100 aa=1;bb=0;cc=0;
+        // #100 aa=1;bb=0;cc=0;
         // #100 aa=1;bb=1;cc=1;
 
         // all transfers
