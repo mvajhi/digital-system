a94ee3d1de448cc1626a8087e55e44392a67dcfd
mahdi vajhi <mvajhimv@gmail.com>
2023-11-07 15:08:56 +0330

    Update

--- ca1/1.sv
+++ ca1/1.sv
@@ -1,31 +1,39 @@
+`timescale 1ns/1ns
 module q1 (output y0, y1, input a,b,c);
     supply0 gnd;
     supply1 vdd;
 
-    wire d1,d2,d3,d4,d5,d6,
-         u1,u2,u3;
+    wire d1,d2,d3,d4,d5,
+         u1,u2,u3,u4,u5;
+
+    //for y1
+    pmos #(5,6,7) p1 (u1, vdd, ~a);
+    pmos #(5,6,7) p2 (u2, vdd, ~b);
+    pmos #(5,6,7) p3 (y1, u1, ~b);
+    pmos #(5,6,7) p4 (y1, u1, ~c);
+    pmos #(5,6,7) p5 (y1, u2, ~c);
+
+    nmos #(3,4,5) n1 (y1, d1, ~b);
+    nmos #(3,4,5) n2 (y1, d1, ~c);
+    nmos #(3,4,5) n3 (d1, gnd, ~a);
+    nmos #(3,4,5) n4 (d1, d2, ~b);
+    nmos #(3,4,5) n5 (d2, gnd, ~c);
 
     //for y0
-    pmos #(3,4,5) p1 (u2, vdd, a);
-    pmos #(3,4,5) p2 (u1, vdd, b) ;
-    pmos #(3,4,5) p3 (u1, vdd, ~c);
-    pmos #(3,4,5) p4 (u2, u1, ~b) ;
-    pmos #(3,4,5) p5 (u2, u1, c);
-    pmos #(3,4,5) p6 (y0, u2, ~a);
-    pmos #(3,4,5) p7 (u3, u2, c);
-    pmos #(3,4,5) p8 (u3, u2, b);
-    pmos #(3,4,5) p9 (y0, u3, ~c);
-    pmos #(3,4,5) p10(y0, u3, ~b);
+    pmos #(5,6,7) p6 (u3, vdd, ~a);
+    pmos #(5,6,7) p7 (u4, vdd, y1);
+    pmos #(5,6,7) p8 (u5, u3, ~b);
+    pmos #(5,6,7) p9 (y0, u5, ~c);
+    pmos #(5,6,7) p10(y0, u4, ~a);
+    pmos #(5,6,7) p11(y0, u4, ~b);
+    pmos #(5,6,7) p12(y0, u4, ~c);
 
-    nmos #(5,6,7) n1 (d1, gnd, a);
-    nmos #(5,6,7) n2 (d2, gnd, ~a);
-    nmos #(5,6,7) n3 (d3, d1, ~c);
-    nmos #(5,6,7) n4 (d4, d2, c);
-    nmos #(5,6,7) n5 (d5, d1, c);
-    nmos #(5,6,7) n6 (d6, d2, ~c);
-    nmos #(5,6,7) n7 (y0, d3, b);
-    nmos #(5,6,7) n8 (y0, d4, b);
-    nmos #(5,6,7) n9 (y0, d5, ~b);
-    nmos #(5,6,7) n10(y0, d6, ~b);
+    nmos #(3,4,5) n6 (y0, d3, ~a);
+    nmos #(3,4,5) n7 (y0, d3, ~b);
+    nmos #(3,4,5) n8 (y0, d3, ~c);
+    nmos #(3,4,5) n9 (d3, gnd, y1);
+    nmos #(3,4,5) n10(d3, d4, ~a);
+    nmos #(3,4,5) n11(d4, d5, ~b);
+    nmos #(3,4,5) n12(d5, gnd, ~c);
     
 endmodule
--- ca1/tb.sv
+++ ca1/tb.sv
@@ -1,9 +1,10 @@
+`timescale 1ns/1ns
 module tb();
     logic aa,bb,cc;
     wire out0,out1;
-    // q1 mygate(.y0(out0),.a(aa),.b(bb),.c(cc));
-    My_OC test(.w(out0),.a(aa),.b(bb),.c(cc));
+    q1 mygate(.y0(out0), .y1(out1), .a(aa), .b(bb), .c(cc));
     initial begin
+        // logic table
         // #100 aa=0;bb=0;cc=0;
         // #100 aa=0;bb=0;cc=1;
         // #100 aa=0;bb=1;cc=0;
@@ -13,17 +14,96 @@ module tb();
         // #100 aa=1;bb=1;cc=0;
         // #100 aa=1;bb=1;cc=1;
 
-        //0 for y0
+        //0 for y1
         #100 aa=0;bb=0;cc=0;
-        #100 aa=0;bb=1;cc=1;
-        #100 aa=1;bb=0;cc=1;
-        #100 aa=1;bb=1;cc=0;
+        // #100 aa=0;bb=0;cc=1;
+        // #100 aa=1;bb=1;cc=0;
+        // #100 aa=1;bb=0;cc=0;
+
+        //1 for y1
+        // #100 aa=0;bb=1;cc=1;
+        // #100 aa=1;bb=1;cc=0;
+        // #100 aa=1;bb=0;cc=1;
+        // #100 aa=1;bb=1;cc=1;
+
+        //0 for y0
+        // #100 aa=0;bb=0;cc=0;
+        // #100 aa=0;bb=1;cc=1;
+        // #100 aa=1;bb=0;cc=1;
+        // #100 aa=1;bb=1;cc=0;
 
         //1 for y0
         // #100 aa=0;bb=0;cc=1;
         // #100 aa=0;bb=1;cc=0;
-        // #100 aa=1;bb=0;cc=0;
+        #100 aa=1;bb=0;cc=0;
         // #100 aa=1;bb=1;cc=1;
+
+        // all transfers
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 0; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 0; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 1;
+        // #100 aa = 1; bb = 1; cc = 0;
+        // #100 aa = 1; bb = 1; cc = 1;
+
         #100 $stop;
     end
 endmodule
