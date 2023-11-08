9d31660559cfc55c7b69add94a4c250f1d58094f
mahdi vajhi <mvajhimv@gmail.com>
2023-11-08 16:01:00 +0330

    Change delay of part 4 with using part 2

--- ca1/q4.sv
+++ ca1/q4.sv
@@ -1,5 +1,5 @@
 `timescale 1ns/1ns
 module q4 (output y0, y1, input a,b,c);
-    assign #(26,22) y0 = a ^ b ^ c;
-    assign #(23,22) y1 = a & b | a & c | b & c;
+    assign #(24,29) y0 = a ^ b ^ c;
+    assign #(15,14) y1 = a & b | a & c | b & c;
 endmodule
