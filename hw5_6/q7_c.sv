ed45fdf895fe7e4b806f8fb105f5ccddb1438149
mahdi vajhi <mvajhimv@gmail.com>
2023-11-27 16:33:19 +0330

    Write q7

---
+++ hw5_6/q7_c.sv
@@ -0,0 +1,5 @@
+module q7_c (input a,b,l,g,e, out lt,gt,eq);
+    assign #17 lt = ~a & b + ~a & l +  b & l;
+    assign #17 gt = a & ~b + a & g  + ~b & g;
+    assign #17 eq = ~a & ~b & e + a & b & e;
+endmodule
---
+++ hw5_6/q7_d.sv
@@ -0,0 +1,13 @@
+module q7_d (input [7:0] a,b, output lt,gt,eq);
+    wire [8:0] l,g,e;
+    genvar i;
+    generate
+        for (i = 7; i >= 0; i = i - 1) begin
+            q7_c cmp (.a(a[i]), .b(b[i]), .lt(l[i + 1]), .gt(g[i+1])
+                        .eq(e[i+1]), .l(l[i]), .g(g[i]), .e(e[i]));
+        end
+    endgenerate
+
+    assign {l[0],e[0],g[0]} = 3'b010;
+    assign {lt,eq,gt} = {l[8],e[8],g[8]};
+endmodule
