25c8b1b31dfbf7dd9835f4568b65230bba844f03
mahdi vajhi <mvajhimv@gmail.com>
2023-11-27 19:01:40 +0330

    Fix q7

--- hw5_6/q7_d.sv
+++ hw5_6/q7_d.sv
@@ -1,9 +1,11 @@
 module q7_d (input [7:0] a,b, output lt,gt,eq);
     wire [8:0] l,g,e;
+    assign aa = {~a[7], a[6:0]};
+    assign bb = {~b[7], b[6:0]};
     genvar i;
     generate
         for (i = 7; i >= 0; i = i - 1) begin
-            q7_c cmp (.a(a[i]), .b(b[i]), .lt(l[i + 1]), .gt(g[i+1])
+            q7_c cmp (.a(aa[i]), .b(bb[i]), .lt(l[i + 1]), .gt(g[i+1])
                         .eq(e[i+1]), .l(l[i]), .g(g[i]), .e(e[i]));
         end
     endgenerate
