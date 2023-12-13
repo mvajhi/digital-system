fd814ce2f5222629095622febd8f942dc7e3d021
mahdi vajhi <mvajhimv@gmail.com>
2023-12-13 17:54:18 +0330

    Fix q1 3in rs_latch

--- ca4/q1.sv
+++ ca4/q1.sv
@@ -3,7 +3,7 @@ module q1_2in (input S, R, output Q, Qb);
     nand #8 N2 (Qb, Q, R);
 endmodule
 
-module q1_3in (input S, R, output Q, Qb);
-    nand #12 N1 (Q, S, S, Qb);
-    nand #12 N2 (Qb, R, R, Q);
+module q1_3in (input S, R, in_s, in_r, output Q, Qb);
+    nand #12 N1 (Q, S, in_s, Qb);
+    nand #12 N2 (Qb, R, in_r, Q);
 endmodule
--- ca4/tbq1.sv
+++ ca4/tbq1.sv
@@ -3,7 +3,7 @@ module tbq1 ();
     wire Q_2, QB_2;
     wire Q_3, QB_3;
     q1_2in rs_latch_2in(S, R, Q_2, QB_2);
-    q1_3in rs_latch_3in(S, R, Q_3, QB_3);
+    q1_3in rs_latch_3in(S, R, 1, 1, Q_3, QB_3);
 
     initial begin
             S = 0; R = 1;
